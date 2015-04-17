2acdfe6dcea8fc7ce2861df9e350fbb2eab71cc7
William Reilly <William.Reilly2010@gmail.com>
2015-04-16 21:51:40 -0400

    Added back the validation of presence of a review (content) of a review.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :video
   belongs_to :user
-  validates_presence_of :rating, :video, :user
+  validates_presence_of :rating, :video, :user, :review
   validates_numericality_of :rating, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
   validates_uniqueness_of :user_id, scope: :video_id
 end
