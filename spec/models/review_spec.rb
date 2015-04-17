2052451c166906d703f86b76b6687bababf026a2
William Reilly <William.Reilly2010@gmail.com>
2015-04-16 23:34:45 -0400

    I had forgotten to add back in the model check for validating presence of review (content) on review model.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,6 +6,7 @@ describe Review do
   it { should validate_presence_of(:rating)}
   it { should validate_presence_of(:video)}
   it { should validate_presence_of(:user)}
+  it { should validate_presence_of :review }
   it { should validate_uniqueness_of(:user_id).scoped_to(:video_id)}
   it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1)}
   it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5)}
