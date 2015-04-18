491c836f6213a84caead766a2a1688140c8a5e1a
William Reilly <William.Reilly2010@gmail.com>
2015-04-18 16:28:29 -0400

    Refactored session_controller_spec. Made tests slimmer.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,45 +5,36 @@ describe SessionsController do
     it "redirects to home_path if logged in" do
       set_current_user
       get :new
-      response.should redirect_to home_path
+      expect(response).to redirect_to home_path
     end
 
     it "renders the new session template if not logged in" do
       get :new
-      response.should render_template :new
+      expect(response).to render_template :new
     end
   end
 
   describe "POST create" do
     context "with valid credentials" do
-      it "sets the session user_id" do
-        user = Fabricate(:user, password: "password")
-        post :create, email: user.email, password: "password"
-        expect(session[:user_id]).to eq(user.id)
-      end
+      let(:user) {Fabricate :user, password: "password"}
 
-      it "redirects to home path" do
-        user = Fabricate(:user, password: "password")
-        post :create, email: user.email, password: "password"
-        response.should redirect_to home_path
-      end
+      before {post :create, email: user.email, password: "password"}
+      it("sets the session user_id") {expect(session[:user_id]).to eq(user.id)}
 
-      it "flashes a success message" do
-        user = Fabricate(:user, password: "password")
-        post :create, email: user.email, password: "password"
-        expect(flash[:success]).not_to be_blank
-      end
+      it("redirects to home path") {response.should redirect_to home_path}
+
+      it("flashes a success message") {expect(flash[:success]).not_to be_blank}
     end
 
     context "with invalid credentials" do
+      let(:user) {Fabricate :user, email: "valid@email.com", password: "valid_password"}
+
       it "redirects to sign_in path with invalid email" do
-        user = Fabricate(:user, email: "valid@email.com", password: "valid_password")
         post :create, email: "invalid@email.com", password: "valid_password"
         expect_redirect_to_sign_in_path
       end
 
       it "redirects to sign_in path with invalid password" do
-        user = Fabricate(:user, password: "password")
         post :create, email: user.email, password: "invalid_password"
         expect_redirect_to_sign_in_path
       end
@@ -61,16 +52,10 @@ describe SessionsController do
       get :destroy
     end
 
-    it "sets session user_id to nil" do
-      expect(session[:user_id]).to eq(nil)
-    end
+    it("sets session user_id to nil") {expect(session[:user_id]).to eq(nil)}
 
-    it "redirects to root path" do
-      response.should redirect_to root_path
-    end
+    it("redirects to root path") {expect(response).to redirect_to root_path}
 
-    it "flashes a success message" do
-      expect(flash[:success]).not_to be_blank
-    end
+    it("flashes a success message") {expect(flash[:success]).not_to be_blank}
   end
 end
