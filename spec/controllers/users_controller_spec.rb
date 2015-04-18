b90a81f2ee0cd3834678ff4a6bfcfc806089ad21
William Reilly <William.Reilly2010@gmail.com>
2015-04-18 15:00:16 -0400

    Refactored spec tests with macros and shared_examples.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,30 +7,37 @@ describe UsersController do
       expect(assigns(:user)).to be_instance_of User
     end
   end
+
   describe "POST create" do
     context "with valid user" do
       before { post :create, user: Fabricate.attributes_for(:user)}
+
       it "sets @user" do
         expect(assigns(:user)).to be_instance_of User
       end
+
       it "creates a valid user" do
         expect(User.all.count).to eq(1)
       end
+
       it "redirects to sign_in path" do
         response.should redirect_to sign_in_path
       end
     end
 
-    context "without valid user" do
+    context "with invalid user" do
       before do
         post :create, user: {email: nil, password: "password", full_name: Faker::Name.name }
       end
+
       it "sets @user" do
         expect(assigns(:user)).to be_instance_of User
       end
+
       it "does not create a new user" do
         expect(User.all.count).to eq(0)
       end
+      
       it "render the new template" do
         response.should render_template :new
       end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe VideosController do
   describe "GET show" do
     context "with authenticated user" do
-      before {session[:user_id] = Fabricate(:user).id}
+      before { set_current_user }
+
       it "sets @video" do
         video = Fabricate(:video)
         get :show, id: video.id
@@ -23,33 +24,33 @@ describe VideosController do
       end
     end
 
-    context "without authenticated user redirect user to sign_in path" do
-      it "redirects user to sign_in path" do
-        video = Fabricate(:video)
-        get :show, id: video.id
-        response.should redirect_to sign_in_path
-      end
+    it_behaves_like "require_sign_in" do
+      let(:action) { get :show, id: Fabricate(:video).id }
     end
   end
 
   describe "POST search" do
     context "with authenticated user" do
-      before {session[:user_id] = Fabricate(:user).id}
+      before { set_current_user }
+
       it "sets the @results variable" do
         video = Fabricate(:video)
         post :search, search_term: video.title
         expect(assigns(:results)).to eq([video])
       end
+
       it "sets the @video instance variable if there is only one result" do
         video = Fabricate(:video)
         post :search, search_term: video.title
         expect(assigns(:video)).to eq(video)
       end
+
       it "renders the show template if there is only one result" do
         video = Fabricate(:video)
         post :search, search_term: video.title
         response.should render_template(:show)
       end
+
       it "otherwise renders the search template" do
         video1 = Fabricate(:video, title: "abc movie")
         video2 = Fabricate(:video, title: "An abc story")
@@ -58,12 +59,8 @@ describe VideosController do
       end
     end
 
-    context "unauthenticated user is redirected to sign_in path" do
-      it "redirects unauthenticated user to sign_in path" do
-        video = Fabricate(:video)
-        post :search, search_term: video.title
-        response.should redirect_to sign_in_path
-      end
+    it_behaves_like "require_sign_in" do
+      let(:action) { post :search, search_term: Fabricate(:video).title }
     end
   end
 end
