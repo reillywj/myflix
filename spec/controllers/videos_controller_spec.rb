e0e13aaab3610a692fb1cd426c7fe834df588375
William Reilly <William.Reilly2010@gmail.com>
2015-04-18 16:39:01 -0400

    Refractored videos_controller_spec to make slimmer.

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,25 +3,16 @@ require 'spec_helper'
 describe VideosController do
   describe "GET show" do
     context "with authenticated user" do
-      before { set_current_user }
+      let(:video) {Fabricate :video}
 
-      it "sets @video" do
-        video = Fabricate(:video)
+      before do
+        set_current_user
         get :show, id: video.id
-        expect(assigns(:video)).to eq(video)
       end
 
-      it "sets @review" do
-        video = Fabricate(:video)
-        get :show, id: video.id
-        expect(assigns(:review).class).to eq(Review)
-      end
-
-      it "renders the show template" do
-        video = Fabricate(:video)
-        get :show, id: video.id
-        response.should render_template(:show)
-      end
+      it("sets @video")               {expect(assigns(:video)).to eq(video)}
+      it("sets @review")              {expect(assigns(:review).class).to eq(Review)}
+      it("renders the show template") {expect(response).to render_template(:show)}
     end
 
     it_behaves_like "require_sign_in" do
@@ -31,31 +22,30 @@ describe VideosController do
 
   describe "POST search" do
     context "with authenticated user" do
-      before { set_current_user }
+      let(:video) {Fabricate :video}
+
+      before do
+        set_current_user
+        post :search, search_term: video.title
+      end
 
       it "sets the @results variable" do
-        video = Fabricate(:video)
-        post :search, search_term: video.title
         expect(assigns(:results)).to eq([video])
       end
 
       it "sets the @video instance variable if there is only one result" do
-        video = Fabricate(:video)
-        post :search, search_term: video.title
         expect(assigns(:video)).to eq(video)
       end
 
       it "renders the show template if there is only one result" do
-        video = Fabricate(:video)
-        post :search, search_term: video.title
-        response.should render_template(:show)
+        expect(response).to render_template(:show)
       end
 
       it "otherwise renders the search template" do
         video1 = Fabricate(:video, title: "abc movie")
         video2 = Fabricate(:video, title: "An abc story")
         post :search, search_term: "abc"
-        response.should render_template(:search)
+        expect(response).to render_template :search
       end
     end
 
